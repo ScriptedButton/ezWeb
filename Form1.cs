0f8220fc84a9efc6e66460bad494aca443904fef
Cole Brooks <macoryx@gmail.com>
2018-05-15 21:34:57 -0400

    Add files via upload
    initial commit

---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ezWeb
+{
+    public partial class Form1 : Form
+    {
+        static string rootDir = Environment.CurrentDirectory;
+        HtmlHandler handler = new HtmlHandler(rootDir);
+        List <string> elementTypes = new List<string>{ "p", "h1", "hr", "nav", "custom", "container"};
+
+        Uri uri = new Uri(rootDir + @"\index.html");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void readFile()
+        {
+            string[] lines = System.IO.File.ReadAllLines(rootDir + "/index.html");
+
+            listBox2.DataSource = lines;
+            webBrowser1.Navigate(uri);
+        }
+
+        void createIndex()
+        {
+                if (checkBox1.Checked)
+                {
+                    handler.createIndex("bootstrap");
+                }
+                else
+                {
+                    handler.createIndex();
+                }
+        }
+
+        public void handleInit()
+        {
+            if (!File.Exists(rootDir + @"\index.html"))
+            {
+                createIndex();
+            }
+            else
+            {
+                DialogResult prompt = MessageBox.Show("Index already exists, would you like to re-create?", "Notice", MessageBoxButtons.YesNo);
+                if (prompt == DialogResult.Yes)
+                {
+                    createIndex();
+                }
+                else
+                {
+                    return;
+                }
+            }
+            readFile();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists(rootDir + @"\index.html"))
+            {
+                // handler.createIndex();
+                MessageBox.Show("Desired index file not located, please initialize it using the Init File button.");
+            }
+            else
+            {
+                readFile();
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                handler.setType(elementTypes[listBox1.SelectedIndex]);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            handler.lineChanger(listBox2.SelectedIndices, true);
+            readFile();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (handler.elementType == "container")
+            {
+                string toWrap = "";
+                foreach (var i in listBox2.SelectedItems)
+                {
+                    toWrap += i.ToString() + "\n";
+                }
+                string formatted = String.Format("<div class=\"container\">\n{0}</div>", toWrap);
+                handler.lineChanger(listBox2.SelectedIndices, true);
+                handler.addElement(formatted);
+                readFile();
+            }
+            else
+            {
+                handler.addElement(textBox1.Text);
+                readFile();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            handleInit();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            handler.lineChanger(listBox2.SelectedIndices, false, textBox1.Text);
+            readFile();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            handler.insertAbove(listBox2.SelectedIndex, textBox1.Text);
+            readFile();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            handler.insertBelow(listBox2.SelectedIndex, textBox1.Text);
+            readFile();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("iexplore", rootDir + @"\index.html");
+        }
+
+        private void listBox2_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (listBox2.SelectedIndex != -1)
+            {
+                textBox1.Text = listBox2.SelectedItem.ToString();
+            }
+        }
+    }
+}
---
+++ HtmlHandler.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ezWeb
+{
+    class HtmlHandler
+    {
+        public string homeDir;
+        public string elementType = "p";
+
+        public HtmlHandler(string HomeDir)
+        {
+            homeDir = HomeDir;
+        }
+
+        public string formatElement(string text)
+        {
+            string elem = String.Format("<{0}>" + text + "</{0}>", elementType);
+            if (elementType == "hr")
+            {
+                elem = "<hr>";
+            }
+            else if (elementType == "nav")
+            {
+                string links = "";
+                links += "<a class=\"nav-item nav-link\" href=\"#\">Features</a>";
+                elem = String.Format("<nav class=\"navbar navbar-expand-lg navbar-light bg-light\">\r\n  <a class=\"navbar-brand\" href=\"#\">Navbar</a>\r\n  <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarNavAltMarkup\" aria-controls=\"navbarNavAltMarkup\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\r\n    <span class=\"navbar-toggler-icon\"></span>\r\n  </button>\r\n  <div class=\"collapse navbar-collapse\" id=\"navbarNavAltMarkup\">\r\n    <div class=\"navbar-nav\">\r\n      {0}\r\n</div>\r\n  </div>\r\n</nav>", links);
+            }
+            else if (elementType == "custom")
+            {
+                elem = text;
+            }
+            else if (elementType == "container")
+            {
+                elem = text;
+            }
+            return elem;
+        }
+
+        public void addElement(string element)
+        {
+            using (FileStream fs = new FileStream(homeDir + "/index.html", FileMode.Append))
+            {
+                using (StreamWriter w = new StreamWriter(fs, Encoding.UTF8))
+                {
+                    string elem = formatElement(element);
+                    w.WriteLine(elem);
+                }
+            }
+        }
+
+        public void lineChanger(ListBox.SelectedIndexCollection line_to_edit, bool remove = false, string newText = "")
+        {
+            List<string> arrLine = File.ReadAllLines(homeDir + "/index.html").ToList();
+            if (remove)
+            {
+                if (line_to_edit.Count > 1)
+                {
+                    for (int i = line_to_edit.Count - 1; i >= 0; i--)
+                    {
+                        arrLine.RemoveAt(i);
+                    }
+                }
+                else
+                {
+                    arrLine.RemoveAt(line_to_edit[0]);
+                }
+            }
+            else
+            {
+                string elem = formatElement(newText);
+                arrLine[line_to_edit[0]] = elem;
+            }
+            File.WriteAllLines(homeDir + "/index.html", arrLine);
+        }
+
+        public void insertAbove(int index, string text)
+        {
+            List<string> arrLine = File.ReadAllLines(homeDir + "/index.html").ToList();
+            string elem = formatElement(text);
+            arrLine.Insert(index, elem);
+            File.WriteAllLines(homeDir + "/index.html", arrLine);
+        }
+
+        public void insertBelow(int index, string text)
+        {
+            List<string> arrLine = File.ReadAllLines(homeDir + "/index.html").ToList();
+            string elem = formatElement(text);
+            arrLine.Insert(index+1, elem);
+            File.WriteAllLines(homeDir + "/index.html", arrLine);
+        }
+
+        public void setType(string type)
+        {
+            elementType = type;
+        }
+
+        public void createIndex(string args="")
+        {
+            if (args == "bootstrap")
+            {
+                string template = homeDir + @"/templates/bootstrap.txt";
+                string[] template_text = System.IO.File.ReadAllLines(template);
+                System.IO.File.WriteAllLines(homeDir + @"/index.html", template_text);
+            }
+            else
+            {
+                string template = homeDir + @"/templates/default.txt";
+                string[] template_text = System.IO.File.ReadAllLines(template);
+                System.IO.File.WriteAllLines(homeDir + @"/index.html", template_text);
+            }
+        }
+    }
+}
